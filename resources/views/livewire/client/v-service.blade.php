7955f476837d6a9620aac3965f27d61e473b91ee
king <glodin.kiese@gmail.com>
2021-11-30 06:42:10 +0100

    home

--- resources/views/livewire/client/v-service.blade.php
+++ resources/views/livewire/client/v-service.blade.php
@@ -1,6 +1,16 @@
 <div class="flex flex-col items-center h-screen py-5" id="services">
     <h1 class="py-10 text-center lg:text-2xl">NOS <b>SERVICES</b></h1>
-    <div class="w-full xl:max-w-7xl bg-red-50">
-        jjdj
+    <div class="grid w-full grid-rows-3 py-5 xl:max-w-7xl">
+        <div class="grid grid-cols-2">
+            <div class="flex flex-col items-center justify-center shadow-xl">
+                <img src="{{asset('assets/vecteurs/20944343.jpg')}}" alt="" class="w-3/4 ">
+            </div>
+            <div class="flex flex-col items-center justify-center px-5">
+                <h1 class="py-10 text-left lg:text-2xl">Formations</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nulla, rerum esse dicta mollitia ipsum maiores nesciunt molestiae praesentium repellat laudantium maxime adipisci corrupti veritatis quos fugit voluptatum sint reprehenderit.
+                </p>
+            </div>
+        </div>
     </div>
 </div>
