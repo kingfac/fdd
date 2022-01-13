e9ec66295df7bb7b085607e3efae1b6fd57f64d0
king <glodin.kiese@gmail.com>
2022-01-13 01:43:45 +0100

    actualite revision et finission

--- resources/views/livewire/client/v-actu.blade.php
+++ resources/views/livewire/client/v-actu.blade.php
@@ -2,13 +2,22 @@
     {{-- Stop trying to control. --}}
     <div class="flex flex-col items-center justify-center gap-10 py-10 white-transparent2">
         <h1 class="py-5 text-center lg:text-2xl">ACTUALITES DU <b>MOMENT</b></h1>
-        <div class="flex items-center justify-center w-full px-10 lg:gap-10 xl:max-w-7xl lg:flex-row xl:px-0">
+        <div class="grid w-full lg:{{(count($actus) > 0) ? 'grid-cols-2' : 'grid-cols-1'}} px-10 lg:gap-10 xl:max-w-7xl xl:px-0 grid-cols-1 gap-2">
             @foreach ($actus as $actu)    
-            <div class="cursor-pointer">
-                @if (Storage::exists('public/actu/'.$actu->id.'.png'))    
-                    <img src="{{asset('storage/actu/'.$actu->id.'.png')}}?{{ rand() }}" alt="Pas d'image pour cette info" srcset="" class="object-cover transition duration-200 transform hover:scale-125">
+            <div class="flex flex-col flex-1 h-full cursor-pointer">
+                @if (strpos($actu->lien, "v=") !== false)
+                    <iframe {{-- width="560" height="315" --}} class="transition duration-200 transform lg:h-full hover:scale-125 h-72 sm:h-96" src="https://www.youtube.com/embed/{{explode('v=', $actu->lien)[1]}}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                @else
+                    @if (Storage::exists('public/actu/'.$actu->id.'.png'))    
+                        <img src="{{asset('storage/actu/'.$actu->id.'.png')}}?{{ rand() }}" alt="Pas d'image pour cette info" srcset="" class="transition duration-200 transform hover:scale-125">
+                    @endif
                 @endif
-                <h1 class="relative py-2 text-center text-white green-transparent -top-10">{{$actu->titre}}</h1>
+                <div class="p-2 bg-white">
+                    <h2 class="font-bold">{{$actu->titre}}</h2>
+                    <p>
+                        {{$actu->descri}}
+                    </p>
+                </div>
             </div>
             @endforeach
         </div>
